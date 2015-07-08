38795b45867c89ccef27a4bfd8bad79e0a8ce48b
Kadir Ozgur <ben.kadir.ozgur@gmail.com>
2015-07-08 20:36:14 +0300

    Rotate demo1

--- core/src/com/turpgames/rotategame/objects/LevelTimer.java
+++ core/src/com/turpgames/rotategame/objects/LevelTimer.java
@@ -3,104 +3,102 @@ package com.turpgames.rotategame.objects;
 import com.turpgames.framework.v0.IDrawable;
 import com.turpgames.framework.v0.effects.flash.FlashEffect;
 import com.turpgames.framework.v0.impl.Text;
-import com.turpgames.framework.v0.util.Color;
 import com.turpgames.framework.v0.util.ShapeDrawer;
+import com.turpgames.rotategame.components.texts.Last5Text;
 import com.turpgames.rotategame.components.texts.NormalText;
-import com.turpgames.rotategame.components.texts.HugeText;
 import com.turpgames.rotategame.controller.MasterPlayController;
 import com.turpgames.rotategame.utils.R;
 
 public class LevelTimer implements IDrawable {
-	private static float FULLSCALE = R.ROWNUMBER * R.BLOCKSIZE + R.LEVELFRAMEOFFSETX * 2;
-	private static float SCALEDLENGTH = 0; 
-	private static float BARWIDTH = 12;
-	private static Color BARCOLOR = R.Colors.BUTTONCOLOR;
+	private static float SCALE = 1;
 	
 	private MasterPlayController parent;
 	
+	private float secondCounter;
 	private float currTime;
-	private int mapTime;
+	private float mapTime;
 	
 	private NormalText timeText;
-	private HugeText last5Text;
-//	private FlashEffect last5Flash;
+	private Last5Text last5Text;
+	private FlashEffect last5Flash;
 	
 	private boolean timerIsStopped;
 	
-	public LevelTimer(int mapTime, MasterPlayController parent) {
+	public LevelTimer(MasterPlayController parent) {
 		this.parent = parent;
-		this.mapTime = mapTime;
 		
 		timeText = new NormalText();
 		timeText.setText("");
-		timeText.setAlignment(Text.HAlignCenter, Text.VAlignBottom);
-		timeText.setPadY(R.MAPOFFSETY + R.BLOCKSIZE * R.ROWNUMBER + 20);
+		timeText.setAlignment(Text.HAlignRight, Text.VAlignBottom);
+		timeText.setPadY(R.MAPOFFSETY + R.BLOCKSIZE * R.ROWNUMBER + 25);
 		timeText.getColor().set(R.Colors.BLOCKCOLOR);
 		//timeText.setAlignment(Text.HAlignCenter, Text.VAlignTop);
 		
-		last5Text = new HugeText();
+		last5Text = new Last5Text();
 		last5Text.setText("");
-		last5Text.setFontScale(5);
+		last5Text.setFontScale(2);
 		last5Text.getColor().set(R.Colors.BLOCKCOLOR);
-		last5Text.getColor().a = 0.65f;
+		last5Text.getColor().a = 0.45f;
 		last5Text.setAlignment(Text.HAlignCenter, Text.VAlignBottom);
-		last5Text.setPadY(R.MAPOFFSETY + R.BLOCKSIZE);
+		last5Text.setPadY(R.MAPOFFSETY);
 		
-//		last5Flash = new FlashEffect(last5Text, R.Colors.BUTTONCOLOR, 7);
-//		last5Flash.setDuration(10000);
+		last5Flash = new FlashEffect(last5Text, R.Colors.LAST5SECCOLOR, 7);
+		last5Flash.setDuration(10000);
+		last5Flash.start();
 		
 		this.timerIsStopped = true;
-	}
-
-	public void reset() {
-		currTime = mapTime;
 	}
 	
 	public void stop() {
 		timerIsStopped = true;
 	}
 	
-	public void start() {
-		start(mapTime);
-	}
-	
-	public void start(int mapTime) {
+	public void start(float mapTime) {
 		this.mapTime = mapTime;
 		this.currTime = mapTime;
-		timerIsStopped = false;
+		this.secondCounter = 1;
+		this.timerIsStopped = false;
 	}
 	
 	public void update(float elapsed) {
 		if (!timerIsStopped) {
 			currTime -= elapsed;
-			SCALEDLENGTH = FULLSCALE * currTime / mapTime;
-			
-			Integer time = (int) currTime;
-			timeText.setText(time.toString());
-			last5Text.setText(time.toString());
-			
-			if (currTime <= 5) {
-//				last5Flash.start();
-			}
+			SCALE = currTime / mapTime;
 			
 			if (currTime < 0) {
 				currTime = 0;
-				parent.timeUp();
+				parent.levelLost();
 			}
+			
+			secondCounter -= elapsed;
+			if (secondCounter < 0) {
+				secondCounter = 1;
+				parent.secondPassed();
+			}
+
+			Integer time = (int) Math.ceil(currTime);
+			timeText.setText(time.toString());
+			last5Text.setText(time.toString());
 		}
 	}
 	
 	@Override
 	public void draw() {
-		if (currTime < 5) {
+		if (currTime < 9) {
 			last5Text.draw();
 		}
-		timeText.draw();
+		//timeText.draw();
 		
-		ShapeDrawer.drawRect(R.LEVELFRAMEOFFSETX + (FULLSCALE - SCALEDLENGTH) / 2, R.LEVELFRAMEOFFSETY, SCALEDLENGTH, BARWIDTH, BARCOLOR, true, false);
-		ShapeDrawer.drawRect(R.LEVELFRAMEOFFSETX + (FULLSCALE - SCALEDLENGTH) / 2, R.LEVELFRAMEOFFSETY + R.BLOCKSIZE * R.ROWNUMBER + BARWIDTH, SCALEDLENGTH, BARWIDTH, BARCOLOR, true, false);
+		// Frame
+		ShapeDrawer.drawRect(R.LEVELFRAMEOFFSETX, R.LEVELFRAMEOFFSETY, R.LEVELFRAMEWIDTH, R.BARWIDTH, R.Colors.LEVELFRAMECOLOR, true, false);
+		ShapeDrawer.drawRect(R.LEVELFRAMEOFFSETX, R.LEVELFRAMEOFFSETY + R.LEVELFRAMEHEIGHT - R.BARWIDTH, R.LEVELFRAMEWIDTH, R.BARWIDTH, R.Colors.LEVELFRAMECOLOR, true, false);
+		ShapeDrawer.drawRect(R.LEVELFRAMEOFFSETX, R.LEVELFRAMEOFFSETY, R.BARWIDTH, R.LEVELFRAMEHEIGHT, R.Colors.LEVELFRAMECOLOR, true, false);
+		ShapeDrawer.drawRect(R.LEVELFRAMEOFFSETX + R.LEVELFRAMEWIDTH - R.BARWIDTH, R.LEVELFRAMEOFFSETY, R.BARWIDTH, R.LEVELFRAMEHEIGHT, R.Colors.LEVELFRAMECOLOR, true, false);
 
-		ShapeDrawer.drawRect(R.LEVELFRAMEOFFSETX, R.LEVELFRAMEOFFSETY + (FULLSCALE - SCALEDLENGTH) / 2, BARWIDTH, SCALEDLENGTH, BARCOLOR, true, false);
-		ShapeDrawer.drawRect(R.LEVELFRAMEOFFSETX + R.BLOCKSIZE * R.ROWNUMBER + BARWIDTH, R.LEVELFRAMEOFFSETY + (FULLSCALE - SCALEDLENGTH) / 2, BARWIDTH, SCALEDLENGTH, BARCOLOR, true, false);
+		// Timer bars
+		ShapeDrawer.drawRect(R.LEVELFRAMEOFFSETX + (1 - SCALE) * R.LEVELFRAMEWIDTH / 2, R.LEVELFRAMEOFFSETY, R.LEVELFRAMEWIDTH * SCALE, R.BARWIDTH, R.Colors.TIMERBARCOLOR, true, false);
+		ShapeDrawer.drawRect(R.LEVELFRAMEOFFSETX + (1 - SCALE) * R.LEVELFRAMEWIDTH / 2, R.LEVELFRAMEOFFSETY + R.LEVELFRAMEHEIGHT - R.BARWIDTH, R.LEVELFRAMEWIDTH * SCALE, R.BARWIDTH, R.Colors.TIMERBARCOLOR, true, false);
+		ShapeDrawer.drawRect(R.LEVELFRAMEOFFSETX, R.LEVELFRAMEOFFSETY + (1 - SCALE) * R.LEVELFRAMEHEIGHT / 2, R.BARWIDTH, R.LEVELFRAMEHEIGHT * SCALE, R.Colors.TIMERBARCOLOR, true, false);
+		ShapeDrawer.drawRect(R.LEVELFRAMEOFFSETX + R.LEVELFRAMEWIDTH - R.BARWIDTH, R.LEVELFRAMEOFFSETY + (1 - SCALE) * R.LEVELFRAMEHEIGHT / 2, R.BARWIDTH, R.LEVELFRAMEHEIGHT * SCALE, R.Colors.TIMERBARCOLOR, true, false);
 	}
 }
